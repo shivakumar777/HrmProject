fbeaefbd473373bafab71121cd486bfd85c8f4aa
shivakumar777 <yellure.shivakumar@gmail.com>
2022-09-02 20:26:54 +0530

    second commit

---
+++ AnilHRMApp/src/test/java/com/hrm/testscripts/AdminTestScripts.java
@@ -0,0 +1,18 @@
+package com.hrm.testscripts;
+
+import org.testng.annotations.Test;
+
+public class AdminTestScripts extends LoginTestScripts{
+	
+
+	    @Test
+	    public void adminScipts(){
+	    	
+	    }
+	    
+}
+
+
+// Admin Serivice I Need to Write the Code..!!
+
+//   In Admin Test Scrips I Need to CALL the Service METHOD..!!
